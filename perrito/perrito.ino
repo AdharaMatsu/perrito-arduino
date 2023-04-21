c73b0f4d034901daf97c2ff1b24cf5ac385ab3b1
Adhara Cavazos <95373792+AdharaMatsu@users.noreply.github.com>
2023-04-21 10:28:28 -0600

    xP

--- perrito/perrito.ino
+++ perrito/perrito.ino
@@ -21,9 +21,10 @@ int pulso, cm;
 const int pin_Servo = 8;
 const int pin_Echo = 11;
 const int pin_Trig = 12;
+String direccion;
 
 void setup() {
-  Serial.begin(38400);
+  Serial.begin(9600);
   pinMode(motorA1, OUTPUT);
   pinMode(motorA2, OUTPUT);
   pinMode(motorB1, OUTPUT);
@@ -44,136 +45,65 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(pin_Trig, LOW);
-  delayMicroseconds(2);
+
+
+  //digitalWrite(pin_Trig, LOW);
+  //delayMicroseconds(2);
   digitalWrite(pin_Trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(pin_Trig, LOW);
+  delayMicroseconds(2);
 
   pulso = pulseIn(pin_Echo, HIGH);
-  cm = pulso / 29 / 2;
-  delay(500);
-  if (cm <= 80) {
-    analogWrite(motorA1, 0);
-    analogWrite(motorA2, 0);
-    analogWrite(motorB1, 0);
-    analogWrite(motorB2, 0);
+  //cm = pulso / 29 / 2;
+  cm = pulso/59;
+  Serial.println(cm);
+  delay(200);
 
-    delay(2000);
+    if (cm <= 25) {
 
-    rotar();
+
+      rotar();
+
+    
+  }else{
+
+    enfrente();
+
   }
-  enfrente();
-  delay(2000);
-  String rnd = prob();
+    
+  }
+
+
+void rotar() {
+  atras();
+  delay(700);//tIEMPO QUE DARA HACIA ATRAS
+  direccion = prob_direccion();
+  if (direccion == "izquierda") {
+    izquierda();
+    delay(1000);//Tiempo que dara hacia la izquierda
+  } else if (direccion == "derecha") {
+    derecha();
+    delay(1000);//Tiempo que dara hacia la derecha
+  }
+}
+
+  void atras() {
+  digitalWrite(motorA2, HIGH);
+  digitalWrite(motorA1, LOW);
+
+  digitalWrite(motorB2, HIGH);
+  digitalWrite(motorB1, LOW);
+
+  analogWrite(motorAspeed, 95);
+  analogWrite(motorBspeed, 95);
+  delay(200);
+    String rnd = prob();
   if (rnd == "mover colita") {
     mover();
   } else {
     ladrar();
   }
-  delay(1500);
-}
-void rotar() {
-  atras();
-  String direccion = prob_direccion();
-  if (direccion == "izquierda") {
-    izquierda();
-  } else if (direccion == "derecha") {
-    derecha();
-  }
-}
-void ladrar() {
-  // Generar un ladrido de perro
-  tone(BUZZER_PIN, LADRIDO_PERROFREQ);
-  delay(LADRIDO_PERRODURATION);
-  noTone(BUZZER_PIN);
-
-  // Esperar un tiempo antes de generar otro ladrido
-  delay(500);
-}
-void mover() {
-  for (int i = 0; i <= 90; i++) {
-    int angle = map(i, 0, 90, 0, 180);  // Mapear el ángulo a los valores de PWM (0 a 180)
-    analogWrite(pin_Servo, angle);      // Generar el pulso PWM para el ángulo actual
-    delay(10);                          // Esperar un breve tiempo para que el servomotor se mueva
-  }
-
-  delay(1000);  // Esperar 1 segundo en la posición de 90 grados
-
-  // Regresar el servomotor a 0 grados
-  for (int i = 90; i >= 0; i--) {
-    int angle = map(i, 0, 90, 0, 180);  // Mapear el ángulo a los valores de PWM (0 a 180)
-    analogWrite(pin_Servo, angle);      // Generar el pulso PWM para el ángulo actual
-    delay(10);                          // Esperar un breve tiempo para que el servomotor se mueva
-  }
-  delay(1000);  //
-}
-String prob() {
-  num_direccion = random(0, 101);
-  if (num_direccion <= 70) {
-    return "mover colita";
-  } else {
-    return "ladrar";
-  }
-}
-void enfrente() {
-  digitalWrite(motorA2, LOW);
-  delay(1);
-  digitalWrite(motorA1, HIGH);
-  delay(1);
-
-  digitalWrite(motorB2, LOW);
-  delay(1);
-  digitalWrite(motorB1, HIGH);
-
-  analogWrite(motorAspeed, 180);
-  analogWrite(motorBspeed, 180);
-}
-
-void derecha() {
-  digitalWrite(motorA1, HIGH);
-  delay(1);
-  digitalWrite(motorA2, LOW);
-  delay(1);
-
-  digitalWrite(motorB1, HIGH);
-  delay(1);
-  digitalWrite(motorB2, LOW);
-
-
-  analogWrite(motorAspeed, 180);
-  analogWrite(motorBspeed, 180);
-  delay(1000);
-}
-
-void izquierda() {
-  digitalWrite(motorA1, LOW);
-  delay(1);
-  digitalWrite(motorA2, HIGH);
-  delay(1);
-
-  digitalWrite(motorB1, LOW);
-  delay(1);
-  digitalWrite(motorB2, HIGH);
-
-  analogWrite(motorAspeed, 180);
-  analogWrite(motorBspeed, 180);
-  delay(1000);
-}
-
-void atras() {
-  digitalWrite(motorA2, HIGH);
-  delay(1);
-  digitalWrite(motorA1, LOW);
-  delay(1);
-
-  digitalWrite(motorB2, LOW);
-  delay(1);
-  digitalWrite(motorB1, HIGH);
-
-  analogWrite(motorAspeed, 150);
-  analogWrite(motorBspeed, 150);
-  delay(1500);
 }
 
 String prob_direccion() {
@@ -183,4 +113,65 @@ String prob_direccion() {
   } else {
     return "derecha";
   }
+}
+
+
+void izquierda() {
+  digitalWrite(motorA1, LOW);
+  digitalWrite(motorA2, HIGH);
+
+  digitalWrite(motorB1, HIGH);
+  digitalWrite(motorB2, LOW);
+
+  analogWrite(motorAspeed, 95);
+  analogWrite(motorBspeed, 95);
+}
+
+  void derecha() {
+  digitalWrite(motorA1, HIGH);
+  digitalWrite(motorA2, LOW);
+
+  digitalWrite(motorB1, LOW);
+  digitalWrite(motorB2, HIGH);
+
+  analogWrite(motorAspeed, 95);
+  analogWrite(motorBspeed, 95);
+}
+
+void enfrente() {
+  
+  digitalWrite(motorA2, LOW);
+  digitalWrite(motorA1, HIGH);
+
+  digitalWrite(motorB2, LOW);
+  digitalWrite(motorB1, HIGH);
+
+  analogWrite(motorAspeed, 95);
+  analogWrite(motorBspeed, 95);
+}
+
+String prob() {
+  num_direccion = random(0, 101);
+  if (num_direccion <= 70) {
+    return "mover colita";
+  } else {
+    return "ladrar";
+  }
+}
+
+
+void ladrar() {
+  // Generar un ladrido de perro
+  tone(BUZZER_PIN, LADRIDO_PERROFREQ);
+  delay(LADRIDO_PERRODURATION);
+  noTone(BUZZER_PIN);
+}
+
+void mover() {
+  for (int i = 0; i <= 90; i++) {
+    int angle = map(i, 0, 90, 0, 178);  // Mapear el ángulo a los valores de PWM (0 a 180)
+    analogWrite(pin_Servo, angle);      // Generar el pulso PWM para el ángulo actual
+    delay(10);                          // Esperar un breve tiempo para que el servomotor se mueva
+  }
+
 }
